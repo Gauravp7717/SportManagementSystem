53594071f4894ad102a6a15651dc012ef5ac15da
yashpatil119 <115876194+yashpatil119@users.noreply.github.com>
2025-11-29 17:18:21 +0530

    resolved confilcts

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,35 +4,36 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 
+
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
 // Middlewares
-// app.use(cors());
-app.use(
-  cors({
-    origin: true, // reflects the request origin
-    credentials: true, // allows cookies
-    methods: "GET,POST,PUT,PATCH,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
-
-app.use(express.json());
+app.use(cors());
+app.use(express.json()); 
 app.use(cookieParser());
+
 
 // Test Route
 app.get("/healthcheck", (req, res) => res.send("ok"));
 
+
+
 //routes import
-import userRouter from "./routes/user.routes.js";
-import adminRouter from "./routes/superAdmin.route.js";
+import userRouter from './routes/user.routes.js'
+
+
+
+
+
 
 //routes handling
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/admin", adminRouter);
+app.use('/api/v1/users',userRouter)
+
+
+
 
 // Server Start
 const port = process.env.PORT || 3000;
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,27 +1,29 @@
 import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import User from "../models/User.model.js";
 
-export const verifyJWT = asyncHandler(async (req, _, next) => {
-  const token =
-    req.cookies?.accessToken ||
-    req.header("Authorization")?.replace("Bearer ", "");
 
-  if (!token) {
-    throw new apiError(401, "Unauthorized request");
-  }
 
-  const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+export const verifyJWT=asyncHandler(async (req,_,next)=>{
+    const token=req.cookies?.accessToken || 
+     req.header("Authorization")?.replace("Bearer ","")
 
-  const user = await User.findById(decodeToken?._id).select(
-    "-password -refreshToken"
-  );
+     if(!token){
+        throw new apiError (401, "Unauthorized request");
+     }
+     const decodeToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
 
-  if (!user) {
-    throw new apiError(401, "Invalid Access Token");
-  }
+     const user= await User.findById(decodeToken?._id).select("-password -refreshToken")
 
-  req.user = user;
-  next();
-});
+     if(!user){
+        throw new apiError(401,"Invalid Access Token")
+     }
+     
+     req.user=user;
+
+     next();
+
+
+
+})
